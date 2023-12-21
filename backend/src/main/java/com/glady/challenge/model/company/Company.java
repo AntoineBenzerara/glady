83d9e4ea21bc7dfad1d5cfbc27507999c9347736
Antoine Benzerara <antoine.benzerara@xandr.com>
2023-12-21 11:08:26 +0100

    Company model and tests for balance checks

--- backend/src/main/java/com/glady/challenge/model/company/Company.java
+++ backend/src/main/java/com/glady/challenge/model/company/Company.java
@@ -1,4 +1,57 @@
 package com.glady.challenge.model.company;
 
+import com.glady.challenge.model.user.User;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Company {
+
+    private int id;
+    private String name;
+    private BigDecimal balance;
+
+    List<User> userList;
+
+    public Company(int id, String name, BigDecimal balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public boolean hasEnoughBalance(BigDecimal amount) {
+        return this.balance.compareTo(amount) >= 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
 }
--- backend/src/test/java/com/glady/challenge/model/company/CompanyTest.java
+++ backend/src/test/java/com/glady/challenge/model/company/CompanyTest.java
@@ -1,15 +1,20 @@
 package com.glady.challenge.model.company;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 public class CompanyTest {
     @Test
     void given_an_amount_balance_check_should_return_false_if_bigger_than_company_balance(){
-
+        Company company = new Company(123, "testCompany",new BigDecimal(1000));
+        Assertions.assertFalse(company.hasEnoughBalance(new BigDecimal(1001)));
     }
 
     @Test
     void given_an_amount_balance_check_should_return_true_if_smaller_than_company_balance(){
-
+        Company company = new Company(123, "testCompany",new BigDecimal(1000));
+        Assertions.assertTrue(company.hasEnoughBalance(new BigDecimal(500)));
     }
 }
