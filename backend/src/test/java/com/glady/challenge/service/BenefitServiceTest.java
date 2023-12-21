d01d4680754831b5b7fd26144a7bb5d3486e415b
Antoine Benzerara <antoine.benzerara@xandr.com>
2023-12-21 12:13:09 +0100

    Removing benefit service test as it is not required

--- backend/src/test/java/com/glady/challenge/service/BenefitServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.glady.challenge.service;
-
-import org.junit.jupiter.api.Test;
-
-public class BenefitServiceTest {
-
-    @Test
-    void given_an_amount_greater_than_benefit_amount_should_not_proceed(){}
-}
