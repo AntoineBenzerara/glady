bf470cf841bf1390ecee6e1ea1516c4d42e99fda
AntoineBenzerara <Benzerara.antoine@gmail.com>
2024-01-04 23:19:15 +0100

    Changing superclass entity to MappedSuperClass
    Avoid having a table created for benefits

--- backend/src/main/java/com/glady/challenge/model/benefit/Benefit.java
+++ backend/src/main/java/com/glady/challenge/model/benefit/Benefit.java
@@ -3,13 +3,15 @@ package com.glady.challenge.model.benefit;
 
 import com.glady.challenge.model.user.User;
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,10 +19,11 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 
-@Entity
+
 @Inheritance(strategy = InheritanceType.JOINED)
 @Data
 @NoArgsConstructor
+@MappedSuperclass
 public abstract class Benefit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,7 +34,8 @@ public abstract class Benefit {
     @Column(name = "emited_on_date")
     private LocalDate emittedOn;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
 }
