852605f3a5885129475c3293110ea13f5bd79934
AntoineBenzerara <Benzerara.antoine@gmail.com>
2024-01-05 10:08:50 +0100

    Adding company service
    Provide both services for adding either a gift or a meal benefit to a user.

--- backend/src/main/java/com/glady/challenge/model/user/User.java
+++ backend/src/main/java/com/glady/challenge/model/user/User.java
@@ -2,6 +2,8 @@ package com.glady.challenge.model.user;
 
 import com.glady.challenge.model.benefit.GiftBenefit;
 import com.glady.challenge.model.benefit.MealBenefit;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,21 +11,25 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @Data
 @NoArgsConstructor
+@Table(name = "glady_user" )
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-    @OneToMany
-    private List<GiftBenefit> giftBenefits;
-    @OneToMany
-    private List<MealBenefit> mealBenefits;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GiftBenefit> giftBenefits = new ArrayList<>();
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MealBenefit> mealBenefits = new ArrayList<>();
 
 }
--- backend/src/main/java/com/glady/challenge/service/CompanyService.java
+++ backend/src/main/java/com/glady/challenge/service/CompanyService.java
@@ -1,89 +1,91 @@
 package com.glady.challenge.service;
 
+import com.glady.challenge.model.benefit.Benefit;
 import com.glady.challenge.model.benefit.GiftBenefit;
 import com.glady.challenge.model.benefit.MealBenefit;
+import com.glady.challenge.model.benefit.dto.GiftBenefitDto;
+import com.glady.challenge.model.benefit.dto.MealBenefitDto;
 import com.glady.challenge.model.company.Company;
 import com.glady.challenge.model.user.User;
 import com.glady.challenge.repository.CompanyRepository;
+import com.glady.challenge.repository.GiftBenefitRepository;
+import com.glady.challenge.repository.MealBenefitRepository;
 import com.glady.challenge.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 
 @Service
 public class CompanyService {
 
-
-    private final Logger LOGGER = LoggerFactory.getLogger(CompanyService.class);
-
     private final CompanyRepository companyRepository;
     private final UserRepository userRepository;
 
-    public CompanyService(@Autowired CompanyRepository companyRepository, @Autowired UserRepository userRepository) {
+    private final GiftBenefitRepository giftBenefitRepository;
+
+    private final MealBenefitRepository mealBenefitRepository;
+
+    public CompanyService(@Autowired CompanyRepository companyRepository, @Autowired UserRepository userRepository, @Autowired GiftBenefitRepository giftBenefitRepository, @Autowired MealBenefitRepository mealBenefitRepository) {
         this.companyRepository = companyRepository;
         this.userRepository = userRepository;
+        this.mealBenefitRepository = mealBenefitRepository;
+        this.giftBenefitRepository = giftBenefitRepository;
     }
 
-    public boolean distributeGiftBenefit(int companyId, int userId, BigDecimal amount){
-        if (controlBenefitRequestIsValid(companyId, userId, amount)) {
-            GiftBenefit giftBenefit = new GiftBenefit(amount, LocalDate.now());
-            //TODO: add both update in a transaction to manage rollback upon failure
-            if (companyRepository.reduceBalance(companyId, amount)) {
-                return userRepository.addGiftBenefit(userId, giftBenefit);
-            } else {
-                LOGGER.error("Failure upon reduce balance for company {} on amount {} ", companyId, amount);
-                return false;
-            }
-        }
-        //Request is invalid
-        return false;
+    public MealBenefitDto distributeMealBenefit(Long companyId, Long userId, BigDecimal amount){
+        return MealBenefitDto.ofMealBenefit((MealBenefit) distributeBenefit(companyId,userId,amount, MealBenefit.class));
     }
 
-    public boolean distributeMealBenefit(int companyId, int userId, BigDecimal amount) {
-        if (controlBenefitRequestIsValid(companyId, userId, amount)) {
-            MealBenefit mealBenefit = new MealBenefit(amount, LocalDate.now());
-            //TODO: add both update in a transaction to manage rollback upon failure
-            if (companyRepository.reduceBalance(companyId, amount)) {
-                return userRepository.addMealBenefit(userId, mealBenefit);
-            } else {
-                LOGGER.error("Failure upon reduce balance for company {} on amount {} ", companyId, amount);
-                return false;
-            }
-        }
-        //Request is invalid
-        return false;
+    public GiftBenefitDto distributeGiftBenefit(Long companyId, Long userId, BigDecimal amount){
+        return GiftBenefitDto.ofGiftBenefit((GiftBenefit) distributeBenefit(companyId,userId,amount, GiftBenefit.class));
     }
 
-    private boolean controlBenefitRequestIsValid(int companyId, int userId, BigDecimal amount) {
+
+    @Transactional
+    public Benefit distributeBenefit(Long companyId, Long userId, BigDecimal amount, Class benefitClass ) {
         if (amount.compareTo(BigDecimal.ZERO) < 0) {
-            //TODO: Handle with exception
-            LOGGER.error("Amount cannot be negative: amount provided {} ", amount);
-            return false;
+            throw new RuntimeException("Amount should be > 0");
         }
 
-        Company company = companyRepository.getCompanyById(companyId).orElse(null);
+        User user = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("No user found with that id"));
+        Company company = companyRepository.findById(companyId).orElseThrow(() -> new NoSuchElementException("No company found with that id"));
 
-        if (company == null) {
-            //TODO: Handle with exception
-            LOGGER.error("No company found with {} id ", companyId);
-            return false;
-        }
-        User user = userRepository.findUserById(userId).orElse(null);
-        if (user == null) {
-            //TODO: Handle with exception
-            LOGGER.error("User is unknown", companyId);
-            return false;
-        }
         if (!company.hasEnoughBalance(amount)) {
-            LOGGER.error("Company {} does not have enough found. Current balance : {} ", company.getName(), company.getBalance());
-            return false;
+            throw new RuntimeException("Company balance is not enough.");
         }
-        return true;
+
+
+        //Instantiating new benefit depending on which class type it is
+        Benefit benefit;
+        JpaRepository benefitRepository;
+        if( benefitClass == MealBenefit.class) {
+            benefit = new MealBenefit();
+            benefitRepository = mealBenefitRepository;
+        } else if (benefitClass == GiftBenefit.class) {
+            benefit = new GiftBenefit();
+            benefitRepository = giftBenefitRepository;
+        }else {
+            throw new RuntimeException("This benefit is not yet supported");
+        }
+
+        benefit.setAmount(amount);
+        benefit.setEmittedOn(LocalDate.now());
+        benefit.setUser(user);
+
+        //Reducing balance for company
+        company.setBalance(company.getBalance().subtract(amount));
+        companyRepository.saveAndFlush(company);
+
+        //Casting to generic benefit. This is only possible as MealBenefit and GiftBenefit have the same parameters
+        return (Benefit) benefitRepository.save(benefit);
+
     }
+
 
 
 }
